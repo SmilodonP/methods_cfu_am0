974f1effbc21a2868525a23d7f8689798a4ee151
Karl Fallenius <smilodon.p@gmail.com>
2024-04-29 01:22:52 -0600

    Initial Commit

--- built_in_methods.rb
+++ built_in_methods.rb
@@ -9,19 +9,42 @@
 "Hello World".downcase
 
 
+# The include method is called on the string object "Hello World"
+# The arguement "Hello" is passed, and .include establishes a boolean that tells wether or not the argument is included in the String.
+# The return value is true 
 "Hello World".include?("Hello")
 
 
+# the end with method is called on the string object "Hello World".
+# The arguement "Hello" is passed. The method determines wether the String object ends with "Hello"
+# The return value is false
 "Hello World".end_with?("Hello")
 
 
+# The method is the same as in the previous code, but the arguement has changed.
+# The return value is true.
 "Hello World".end_with?("rld")
 
 
-12.even?
+# ".even?" is a method that determines wether or not an integer is odd or even and returns a boolean value.
+# Before this method can be called, the ".length" method must be used to call the string, "Hello World".
+# This method will express the number of characters in the string as an an integer.
+# The integer provides the ".even?" method with the correct data type.
+# This can be written as:
+`"Hello World".length.even?`
+# The return value is false
+"Hello World".even?
 
 
-18.next
+
+# The next method is called on the string object "Hello World"
+# No arguement is passed. This method simply changes the last letter in the string to the next letter in the alphabet
+# The return value is "Hello Worle"
+# When called on an integer, next increases its value by 1.
+```18.next
+=>19```
+"Hello World".next
+
 
 
 
@@ -40,6 +63,21 @@ first_name = "Jeff"
 puts first_name.start_with?("J")
 
 
+# MY TURN
+# 1. The reverse! method is called on the favorite_food variable, which stores the string object "Milk Steak".
+# No arguement is being passed.
+# The method reverses the contents of the string.
+# The puts command prints the return value of the reverse! method ("kaetS kliM") to the console.
+favorite_food = "Milk Steak"
+puts favorite_food.reverse!
+
+# 2. The swapcase method upcases each downcase character and downcases each upcase character.
+# The method is called on the variable day_of_week, whis stores the object "Today is Opposite Day!"
+# The puts command prints the return value of the swapcase method ("tODAY IS oPPOSITE dAY!") to the console.
+day_of_week = "Today is Opposite Day!"
+puts day_of_week.swapcase
+
+
 
 # SECTION 3: Calling methods on variables assigned to integers.
 # Declare 2 variables assigned to integer objects.
@@ -47,10 +85,32 @@ puts first_name.start_with?("J")
 # https://ruby-doc.org/core-3.1.0/Integer.html
 # Include comments above each method call explaining the impact and return value of that method.
 
+# Calling the div method returns the integer result of dividing the integer value of the variable by the integer value presented in the arguenent (3).
+# The puts command prints the return value (After she died, the old lady's 2 sons and 1 daughter each received 3 of her cats whom she had cherished more than any of her blood relatives) to the console.
+# Each of her childeren inherit 3 cats each.
+old_ladys_cats = 9
+puts "After she died, the old lady's 2 sons and 1 daughter each received #{old_ladys_cats.div(3)} of her cats whom she had cherished more than any of her blood relatives"
 
+# Calling the pred method returns the integer predecessor of the integer assigned to the variable
+# Return prints the return value (69) to the console.
+# No arguement is passed.
+the_number_that_comes_after = 70
+the_number_that_comes_after.pred
 
 # SECTION 4: Calling methods on variables assigned to arrays.
 # Declare 2 variables assigned to arrays.
 # Call a different built-in Ruby method on each of your variables. 
 # https://ruby-doc.org/core-3.1.0/Array.html
 # Include comments above each method call explaining the impact and return value of that method.
+
+# The sort method returns all elements in ascending order.
+# Return prints an organized array ([1, 3, 4, 6, 7, 13, 100]) 
+favorite_numbers = [3, 7, 13, 6, 1, 4, 100]
+puts favorite_numbers.sort
+
+# The sample method returns one or more random elements
+# The arguement (3) defines the number of random elements returned 
+# Return prints the argued number of random elements ([6, 1001, 389])
+randomish_numbers = [33, 51, 6, 9873, 389, 1001, 27534]
+randomish_numbers.sample(3)
+
--- final_practice.rb
+++ final_practice.rb
@@ -5,35 +5,86 @@
 
 
 # 1: Write a method named greeting that returns a string with a general greeting. 
+def general_greeting
+    "Welcome, friends & enemies."
+end
 
 # What is the return value of your method?
-# How many arguments did you pass your method?
+"Welcome, friends & enemies."
 
+# How many arguments did you pass your method?
+# None
 
 # 2: Write a method named custom_greeting that returns a greeting WITH a specific name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def custom_greeting_for_a_cat(name)
+    "Hello #{name}! Arent you just a silly little ball of fur and weirdness!"
+end
 
+name =  "Tina"
+custom_greeting_for_a_cat(name)
+# What is the return value of your method?
+# "Hello Tina! Arent you just a silly little ball of fur and weirdness!"
+
+# How many arguments did you pass your method?
+# 1
+
+# What data type was your argument(s)?
+# String
 
 # 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
-# What data type was your argument(s)?
+def greet_person(first_name, middle_name, last_name)
+    "Hello, #{first_name} #{middle_name} #{last_name}. Isn't it werid that I know your whole name?"
+end
 
+first_name = "Thomas"
+middle_name = "Jonathan"
+last_name = "Steve"
+
+greet_person(first_name, middle_name, last_name)
+
+# What is the return value of your method?
+# "Hello, Thomas Jonathan Steve. Isn't it werid that I know your whole name?"
+
+# How many arguments did you pass your method?
+# 3
+
+# What data type was your argument(s)?
+# Strings
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
 
+def square_it_up(number)
+    number**(2)
+end
+
+number = 11
+puts "The square of #{number} is #{square_it_up(number)}"
+
+
 # What is the return value of your method?
+# The square of the variable integer.
 # How many arguments did you pass your method?
+# 1
 # What data type was your argument(s)?
+# integer
 
 
 # 5: Write a method named check_stock that satisfies the following interaction pattern:
 # Hint: You will only write one check_stock method that checks the quantity and then prints the corresponding statement.
+
+def check_stock(quantity, item)
+   if quantity >= 4
+     "#{item} is stocked"
+   elsif quantity <=3 && quantity >=1
+    "#{item} - running LOW"
+   else
+    "#{item} - OUT of stock!"
+   end
+end
+
 
 check_stock(4, "Coffee");
 # => "Coffee is stocked"
